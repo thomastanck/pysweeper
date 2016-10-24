00a9abca822e806eb29821fb9fee78438ae69361
Thomas Tan <thomastanck@gmail.com>
2016-10-24 14:24:08 +0800

    make @gamemode more generic to fix a bug

--- pysweep/util.py
+++ pysweep/util.py
@@ -1,9 +1,9 @@
 # Little decorator for ensuring the gamemode is enabled
 def gamemode(game_mode_name):
     def _allow_events(f):
-        def wrapper(self, hn, e):
+        def wrapper(self, *args, **kwargs):
             if self.gamemodeselector.is_enabled(game_mode_name):
-                return f(self, hn, e)
+                return f(self, *args, **kwargs)
             else:
                 return
         return wrapper
